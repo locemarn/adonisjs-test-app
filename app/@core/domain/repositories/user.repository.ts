8b5e8187453365ad9d673b7790f6cc6e487ff671
Marcelo Nogueira <marcelon27@gmail.com>
2025-07-02 19:56:02 -0300

    [Feat] - @core/infra layer -> adding new methods on in-memory db and writing unit tests to simulate userInterface db.

--- app/@core/domain/repositories/user.repository.ts
+++ app/@core/domain/repositories/user.repository.ts
@@ -5,4 +5,7 @@ export interface UserRepository {
   getUserByEmail(email: string): Promise<User | null>
   getUserByUsername(username: string): Promise<User | null>
   create(user: User): Promise<User>
+  delete(id: string): Promise<void>
+  updateUsername(id: string, username: string): Promise<User>
+  updatePassword(id: string, password: string): Promise<User>
 }
--- app/@core/infra/db/in_memory/users_in_memory.repository.ts
+++ app/@core/infra/db/in_memory/users_in_memory.repository.ts
@@ -2,7 +2,28 @@ import { User } from '#domain/entities/user.entity'
 import { UserRepository } from '#domain/repositories/user.repository'
 
 export class UsersInMemoryRepository implements UserRepository {
-  users: User[] = []
+  private users: User[] = []
+
+  updateUsername(id: string, username: string): Promise<User> {
+    const user = this.users.find((u) => u.id === id)
+    if (!user) {
+      throw new Error('User not found')
+    }
+    user.props.username = username
+    return Promise.resolve(user)
+  }
+  updatePassword(id: string, password: string): Promise<User> {
+    const user = this.users.find((u) => u.id === id)
+    if (!user) {
+      throw new Error('User not found')
+    }
+    user.props.password = password
+    return Promise.resolve(user)
+  }
+  delete(id: string): Promise<void> {
+    this.users = this.users.filter((user) => user.id !== id)
+    return Promise.resolve()
+  }
 
   getUserById(id: string): Promise<User | null> {
     return Promise.resolve(this.users.find((user) => user.id === id) ?? null)
