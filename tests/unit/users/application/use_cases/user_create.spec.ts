44850e7e74909853708f4b7ea6f353c0eb36c486
Marcelo Nogueira <marcelon27@gmail.com>
2025-07-03 14:58:56 -0300

    restore
    undo changes

--- tests/unit/users/application/use_cases/user_create.spec.ts
+++ tests/unit/users/application/use_cases/user_create.spec.ts
@@ -2,24 +2,22 @@ import { test } from '@japa/runner'
 import { CreateUserUseCase } from '#application/use_cases/users/create_user.use_case'
 import { UsersInMemoryRepository } from '#infra/db/in_memory/users_in_memory.repository'
 import { User } from '#domain/entities/user.entity'
-import { UserRedisRepository } from '#infra/db/redis/user_redis.repository'
 
 const userProps = {
-  username: 'usertest1',
-  email: 'usertest1@gmail.com',
+  username: 'usertest',
+  email: 'usertest@gmail.com',
   password: 'password',
 }
 
 test.group('Users application use cases user create', () => {
   test('should create a new user', async ({ assert }) => {
-    // const userRepo = new UsersInMemoryRepository()
-    const cashRepo = new UserRedisRepository()
-    const createUserUseCase = new CreateUserUseCase(cashRepo)
+    const userRepo = new UsersInMemoryRepository()
+    const createUserUseCase = new CreateUserUseCase(userRepo)
     const user = await createUserUseCase.execute(userProps)
 
     assert.instanceOf(user, User)
-    // const users = cashRepo.getUsers()
-    // assert.lengthOf(users, 1)
-    // assert.deepEqual(users[0].props, userProps)
+    const users = userRepo.getUsers()
+    assert.lengthOf(users, 1)
+    assert.deepEqual(users[0].props, userProps)
   })
 })
