b1cf85f9ed5a095d69316e516a1743e6c5cbf3bd
Marcelo Nogueira <marcelon27@gmail.com>
2025-07-03 22:13:16 -0300

    [Config]: Lucid config database and add postgres server in docker-compose file.

--- adonisrc.ts
+++ adonisrc.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   | will be scanned automatically from the "./commands" directory.
   |
   */
-  commands: [() => import('@adonisjs/core/commands')],
+  commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands')],
 
   /*
   |--------------------------------------------------------------------------
@@ -46,6 +46,7 @@ export default defineConfig({
     () => import('@adonisjs/core/providers/vinejs_provider'),
     () => import('@adonisjs/cors/cors_provider'),
     () => import('@adonisjs/redis/redis_provider'),
+    () => import('@adonisjs/lucid/database_provider')
   ],
 
   /*
