e552bb30a1e6f5805513402277b0dbfc17deb20a
Marcelo Nogueira <marcelon27@gmail.com>
2025-07-05 16:28:16 -0300

    [Fix]: Fixing redis repository

--- app/@core/infra/db/redis/user_redis.repository.ts
+++ app/@core/infra/db/redis/user_redis.repository.ts
@@ -1,10 +1,10 @@
 import { User } from '#domain/entities/user.entity'
-import { UserRepository } from '#domain/repositories/user.repository'
+import { UserRepositoryInterface } from '#domain/repositories/user.repository'
 import redis from '@adonisjs/redis/services/main'
 
-export class UserRedisRepository implements UserRepository {
-  async getUserById(id: string): Promise<User | null> {
-    const user = await redis.get(id)
+export class UserRedisRepository implements UserRepositoryInterface {
+  async getUserById(id: number): Promise<User | null> {
+    const user = await redis.get(id.toString())
     if (!user) {
       return null
     }
@@ -25,28 +25,31 @@ export class UserRedisRepository implements UserRepository {
     return JSON.parse(user)
   }
   async create(user: User): Promise<User> {
-    await redis.set(user.id, JSON.stringify(user))
+    if (!user.id) {
+      throw new Error('User ID is required')
+    }
+    await redis.set(user.id.toString(), JSON.stringify(user))
     return user
   }
-  async delete(id: string): Promise<void> {
-    await redis.del(id)
+  async delete(id: number): Promise<void> {
+    await redis.del(id.toString())
   }
-  async updateUsername(id: string, username: string): Promise<User> {
+  async updateUsername(id: number, username: string): Promise<User> {
     const user = await this.getUserById(id)
     if (!user) {
       throw new Error('User not found')
     }
     user.props.username = username
-    await redis.set(id, JSON.stringify(user))
+    await redis.set(id.toString(), JSON.stringify(user))
     return user
   }
-  async updatePassword(id: string, password: string): Promise<User> {
+  async updatePassword(id: number, password: string): Promise<User> {
     const user = await this.getUserById(id)
     if (!user) {
       throw new Error('User not found')
     }
     user.props.password = password
-    await redis.set(id, JSON.stringify(user))
+    await redis.set(id.toString(), JSON.stringify(user))
     return user
   }
 }
